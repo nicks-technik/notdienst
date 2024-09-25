fcb6515970cd34a95b22fde56451b0fe7de44dc8
Nick <gs@hornick.de>
2024-09-25 02:48:56 +0200

    working

---
+++ startNotdienst.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~/dev/NotdienstWeb
+cd html
+python -m http.server -d . -b 0.0.0.0 8080
+
+cd ..
+source ./.venv/bin/activate
+
+while true; do
+    python NotdienstWeb.py
+    # sleep 900 # Sleep for 5 minutes (300 seconds)
+done
